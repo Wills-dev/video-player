c7f85f0978776a1e493fc8d3a561dbcfd6899a31
Wills <ejioguvictor6@gmail.com>
2022-08-10 05:52:28 +0100

    update changes

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector('.volume-range');
+const volumeBar = document.querySelector('.volume-bar');
+const currentTime = document.querySelector('.time-elapsed');
+const duration = document.querySelector('time-duration');
+const fullScreenBtn = document.querySelector('.fullscreen');
 
 // Play & Pause ----------------------------------- //
 
+const showPlayIcon = () => {
+        playBtn.classList.replace('fa-pause', 'fa-play');
+        playBtn.setAttribute('title', 'Play');
+}
 
-
+const togglePlay = () => {
+    if (video.paused){
+        video.play();
+        playBtn.classList.replace('fa-play', 'fa-pause');
+        playBtn.setAttribute('title', 'Pause');
+    }else {
+        video.pause();
+        showPlayIcon();
+    }
+}
+ 
+// on video end, show play icon
+video.addEventListener('ended', showPlayIcon);
 // Progress Bar ---------------------------------- //
 
 
@@ -18,4 +43,6 @@
 
 // Fullscreen ------------------------------- //
 
-
+// Event listeners
+playBtn.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
